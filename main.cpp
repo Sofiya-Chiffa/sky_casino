96d428b3c575147389e6904f4c836ab080165c57
Sofiya-Chiffa <94921852+Sofiya-Chiffa@users.noreply.github.com>
2023-02-01 13:26:05 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface.Screen();
+            UserInterface.Game();
+            Console.ReadKey();
+        }
+    }
+}
